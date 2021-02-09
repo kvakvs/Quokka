27e7e70217831828126d0a72cfedd0c9b3fd142d
dmytro lytovchenko <dmytro.lytovchenko@gmail.com>
2021-02-09 02:51:34 +0100

    Drawing in the window; Exit menu

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,21 @@ fn main() {
   let system = crate::ui::startup::init(WINDOW_TITLE);
 
   system.main_loop(move |run, ui| {
-    quokka_menubar(ui, &mut state);
+    quokka_menubar(ui, run, &mut state);
     quokka_cluster_view(ui, &mut state)
   });
 }
 
-fn quokka_menubar(ui: &mut Ui, app: &mut QkApp) {
+fn quokka_menubar(ui: &mut Ui, run: &mut bool, app: &mut QkApp) {
   ui.main_menu_bar(|| {
     //---------------------
     // Test Menu
     //---------------------
+    ui.menu(im_str!("Quokka"), true, || {
+      if MenuItem::new(im_str!("Exit")).build(ui) {
+        *run = false;
+      }
+    });
     ui.menu(im_str!("Test"), true, || {
       if MenuItem::new(im_str!("Load Eflame2 Test"))
           .shortcut(im_str!("Alt+1"))
@@ -48,5 +53,37 @@ fn quokka_cluster_view(ui: &mut Ui, app: &mut QkApp) {
         //   "Mouse Position: ({:.1},{:.1})",
         //   mouse_pos[0], mouse_pos[1]
         // ));
+
+        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+        let draw_list = ui.get_window_draw_list();
+        // Will draw channel 0 first, then channel 1, whatever the order of
+        // the calls in the code.
+        //
+        // Here, we draw a red line on channel 1 then a white circle on
+        // channel 0. As a result, the red line will always appear on top of
+        // the white circle.
+        draw_list.channels_split(2, |channels| {
+          const RADIUS: f32 = 100.0;
+          let canvas_pos = ui.cursor_screen_pos();
+          channels.set_current(1);
+          draw_list
+              .add_line(
+                canvas_pos,
+                [canvas_pos[0] + RADIUS, canvas_pos[1] + RADIUS],
+                RED,
+              )
+              .thickness(5.0)
+              .build();
+
+          channels.set_current(0);
+          let center = [canvas_pos[0] + RADIUS, canvas_pos[1] + RADIUS];
+          draw_list
+              .add_circle(center, RADIUS, WHITE)
+              .thickness(10.0)
+              .num_segments(50)
+              .build();
+        });
       });
 }
